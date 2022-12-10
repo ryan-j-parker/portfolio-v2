7650d182cdee66e497176519b41054f9f72c3a43
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-12-10 15:06:38 -0800

    add to alt scene, adjust mesh properties in /scene

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -17,7 +17,7 @@ export default function Main() {
           <div className="div8"><img className="icon" title="Supabase" src="supabase-icon.png" /></div>
           <div className="div9"><h3>I&apos;m currently developing with React.js, Express.js, and Three.js.</h3></div>
         </div> */}
-        
+
         {/* this is the new experimental grid */}
         <div className="parent">
           <div className="div1">1</div>
@@ -31,24 +31,46 @@ export default function Main() {
               alt="image of a smiling man standing in front of foliage"
             />
           </div>
-          <div className="div5"><a href="/mouse"><img src="/mouse/mouse1.jpg" className="mouse-main" /></a></div>
+          <div className="div5">
+            <a href="/mouse">
+              <img src="/mouse/mouse1.jpg" className="mouse-main" />
+            </a>
+          </div>
           <div className="div6">6</div>
           <div className="div7">
             <h1 className="hi">Hi!</h1>
             <h2 className="ryan">My name is Ryan Parker.</h2>
           </div>
-          <div className="div8"><h3 className="creative">I&apos;m a creative software developer located in Vancouver, WA.</h3></div>
-          <div className="div9"><h3 className="mousecode">You can usually find me writing code with Mouse.</h3></div>
+          <div className="div8">
+            <h3 className="creative">
+              I&apos;m a creative software developer located in Vancouver, WA.
+            </h3>
+          </div>
+          <div className="div9">
+            <h3 className="mousecode">You can usually find me writing code with Mouse.</h3>
+          </div>
           <div className="div10">10</div>
           <div className="div11">11</div>
           <div className="div12">12</div>
           <div className="div13">
-            <a href="http://github.com/ryan-j-parker" target="_blank" rel="noreferrer"><img src="/github-icon-white.png" className="icon" /></a>
-            <a href="http://linkedin.com/in/ryanparkerdev" target="_blank" rel="noreferrer"><img src="/linkedin-icon.png" className="icon" /></a>
-            <a href="http://twitter.com/ryanparkerdev" target="_blank" rel="noreferrer"><img src="/twitter-icon.png" className="icon" /></a>
+            <a href="http://github.com/ryan-j-parker" target="_blank" rel="noreferrer">
+              <img src="/github-icon-white.png" className="icon" />
+            </a>
+            <a href="http://linkedin.com/in/ryanparkerdev" target="_blank" rel="noreferrer">
+              <img src="/linkedin-icon.png" className="icon" />
+            </a>
+            <a href="http://twitter.com/ryanparkerdev" target="_blank" rel="noreferrer">
+              <img src="/twitter-icon.png" className="icon" />
+            </a>
           </div>
         </div>
       </div>
+      <a href="/scene">
+        <button className="scene-btn">to 3d sandbox</button>
+      </a>
+      <a href="/alt">
+        <button className="alt-btn">to sandbox v2</button>
+      </a>
     </>
   );
 }
